f57c7efd69331d12a889329864b23d5699ce98e4
Joseph Abbey <joseph@abbey1.org.uk>
2022-08-23 09:12:07 +0100

    more styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 
+import resetStyles from './myreset.css';
 import styles from './index.css';
 import articleStyles from './components/Article.css';
 import figureStyles from './components/Figure.css';
@@ -16,8 +17,19 @@ export default class App extends React.Component {
                         content="width=device-width, initial-scale=1"
                     />
                     <meta name="theme-color" content="#ffdb14" />
+                    <style
+                        dangerouslySetInnerHTML={{
+                            __html: ':root{--theme-color:#ffdb14;}',
+                        }}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: 'window.addEventListener("scroll",()=>{document.body.style.setProperty("--scrollY", scrollY);document.body.style.setProperty("--scrollHeight", document.body.scrollHeight-innerHeight);})',
+                        }}
+                    />
                     <meta name="description" content="Maths Blog" />
                     <title>Maths Blog</title>
+                    <style dangerouslySetInnerHTML={{ __html: resetStyles }} />
                     <style dangerouslySetInnerHTML={{ __html: styles }} />
                     <style
                         dangerouslySetInnerHTML={{ __html: articleStyles }}
--- src/components/Article.js
+++ src/components/Article.js
@@ -4,37 +4,46 @@ import withRouter from '../withRouter';
 import Figure from './Figure';
 
 import * as articles from '../articles/';
+import { Link } from 'react-router-dom';
 
 export class Article extends React.Component {
     render() {
         const Content = articles[this.props.router.params.article];
         return (
             <div className={(this.props.className || '') + ' Article'}>
-                <div>
-                    <Content
-                        components={{
-                            Data: ({ title, author, time }) => (
-                                <React.Fragment>
-                                    <h1 className="title">{title}</h1>
-                                    <i className="reading-time">
-                                        Reading time: {time}
-                                    </i>
-                                    <i className="author">
-                                        By {author}
-                                        <img
-                                            alt=""
-                                            src={
-                                                'https://github.com/' +
-                                                author +
-                                                '.png?size=20'
-                                            }
-                                        />
-                                    </i>
-                                </React.Fragment>
-                            ),
-                            Figure,
-                        }}
-                    />
+                <div className="progress"></div>
+                <div className="main">
+                    <article>
+                        <Content
+                            components={{
+                                Data: ({ title, author, time }) => (
+                                    <React.Fragment>
+                                        <h1 className="title">{title}</h1>
+                                        <i className="reading-time">
+                                            Reading time: {time}
+                                        </i>
+                                        <Link
+                                            to={'//github.com/' + author + '/'}
+                                            about="Authors GitHub profile"
+                                        >
+                                            <i className="author">
+                                                By {author}
+                                                <img
+                                                    alt=""
+                                                    src={
+                                                        '//github.com/' +
+                                                        author +
+                                                        '.png?size=20'
+                                                    }
+                                                />
+                                            </i>
+                                        </Link>
+                                    </React.Fragment>
+                                ),
+                                Figure,
+                            }}
+                        />
+                    </article>
                 </div>
             </div>
         );
