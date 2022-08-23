2af38e06861a7a7946efb5ea92ad61b439d562df
Joseph Abbey <joseph@abbey1.org.uk>
2022-08-23 20:37:12 +0100

    embeds

--- webpack.config.js
+++ webpack.config.js
@@ -5,77 +5,99 @@ const remarkGfm = import('remark-gfm');
 const remarkMath = import('remark-math');
 const rehypeKatex = import('rehype-katex');
 const rehypeHighlight = import('rehype-highlight');
+const remarkEmbedder = import('@remark-embedder/core');
+const oEmbedTransformer = import('@remark-embedder/transformer-oembed');
 
 module.exports = Promise.all([
     remarkGfm,
     remarkMath,
     rehypeKatex,
     rehypeHighlight,
-]).then(([remarkGfm, remarkMath, rehypeKatex, rehypeHighlight]) => ({
-    target: 'node',
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        publicPath: '',
-        filename: 'bundle.js',
-        library: { name: 'bundle', type: 'umd' },
-        globalObject: 'this',
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.css/,
-                use: ['css-loader', 'postcss-loader'],
-            },
-            {
-                test: /\.mdx$/,
-                use: [
-                    'babel-loader',
-                    {
-                        loader: '@mdx-js/loader',
-                        /** @type {import('@mdx-js/loader').Options} */
-                        options: {
-                            jsx: true,
-                            remarkPlugins: [
-                                remarkGfm.default,
-                                remarkMath.default,
-                            ],
-                            rehypePlugins: [
-                                rehypeKatex.default,
-                                rehypeHighlight.default,
-                            ],
+    remarkEmbedder,
+    oEmbedTransformer,
+]).then(
+    ([
+        remarkGfm,
+        remarkMath,
+        rehypeKatex,
+        rehypeHighlight,
+        remarkEmbedder,
+        oEmbedTransformer,
+    ]) => ({
+        target: 'node',
+        mode: 'development',
+        entry: './src/index.js',
+        output: {
+            publicPath: '',
+            filename: 'bundle.js',
+            library: { name: 'bundle', type: 'umd' },
+            globalObject: 'this',
+        },
+        devtool: 'inline-source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader'],
+                },
+                {
+                    test: /\.css/,
+                    use: ['css-loader', 'postcss-loader'],
+                },
+                {
+                    test: /\.mdx$/,
+                    use: [
+                        'babel-loader',
+                        {
+                            loader: '@mdx-js/loader',
+                            /** @type {import('@mdx-js/loader').Options} */
+                            options: {
+                                jsx: true,
+                                remarkPlugins: [
+                                    remarkGfm.default,
+                                    remarkMath.default,
+                                    [
+                                        remarkEmbedder.default.default,
+                                        {
+                                            transformers: [
+                                                oEmbedTransformer.default
+                                                    .default,
+                                            ],
+                                        },
+                                    ],
+                                ],
+                                rehypePlugins: [
+                                    rehypeKatex.default,
+                                    rehypeHighlight.default,
+                                ],
+                            },
                         },
-                    },
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
+                    ],
+                },
+                {
+                    test: /\.(png|jpe?g|gif)$/i,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                        },
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new StaticSiteGeneratorPlugin(
+                'bundle.js',
+                ['/', '/404.html', '/about_us/', '/articles/'].concat(
+                    fs
+                        .readdirSync('./src/articles')
+                        .filter((a) => a !== 'index.js')
+                        .map((a) => '/articles/' + a)
+                )
+            ),
         ],
-    },
-    plugins: [
-        new StaticSiteGeneratorPlugin(
-            'bundle.js',
-            ['/', '/404.html', '/about_us/', '/articles/'].concat(
-                fs
-                    .readdirSync('./src/articles')
-                    .filter((a) => a !== 'index.js')
-                    .map((a) => '/articles/' + a)
-            )
-        ),
-    ],
-    node: {
-        __dirname: true,
-    },
-}));
+        node: {
+            __dirname: true,
+        },
+    })
+);
