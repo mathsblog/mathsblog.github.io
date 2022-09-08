586c11e79322b21b0a482f8d66a8eed422f3922d
Joseph Abbey <joseph@abbey1.org.uk>
2022-09-08 14:00:55 +0100

    Update index.js

--- src/articles/index.js
+++ src/articles/index.js
@@ -1,3 +1,3 @@
 export * as Fibonacci from './Fibonacci/index.mdx';
 export * as Topology from './Topology/index.mdx';
-export * as PascalTriangle from './PascalTriangle/index.mdx';
+// export * as PascalTriangle from './PascalTriangle/index.mdx';
