df9cc4cd30e20a69f055e9f7e0170e1ef0ed6f2d
Aditya <aditya.chorghade@outlook.com>
2025-08-30 17:46:48 +0100

    rebranded to science

--- src/components/Index.js
+++ src/components/Index.js
@@ -9,14 +9,14 @@ export default class Index extends React.Component {
                 <img src={IntroImage} alt="Images of famous mathematician" />
 
                 <article className="intro">
-                    <h1>Welcome to Maths Blog!</h1>
+                    <h1>Welcome to Science Blog!</h1>
                     <p>
-                        Hello and welcome to maths blogs. We are a group of
-                        students passionate about mathematics who want to share
+                        Hello and welcome to science blogs. We are a group of
+                        students passionate about science who want to share
                         this with the world. On this website, you can learn
-                        about exciting mathematical content that is explained in
+                        about exciting scientific content that is explained in
                         a non-traditional way. Our goal with this is to make
-                        mathematics more accessible and enjoyable for people
+                        science more accessible and enjoyable for people
                         around the world! Click on "articles" at the top to
                         check out some of our latest content!
                     </p>
